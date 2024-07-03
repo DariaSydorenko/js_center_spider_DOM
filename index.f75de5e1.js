97ae0732a5cdf5d4de827ca02e3435aed5da0249
Daria Sydorenko <dashasidorenko123@gmail.com>
2024-07-03 14:07:50 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const topp = (wallHeight - spiderHeight) / 2;
+const left = (wallWidth - spiderWidth) / 2;
+spider.style.top = `${topp}px`;
+spider.style.left = `${left}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
